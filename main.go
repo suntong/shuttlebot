6cfaa16de46714d66d1555c5235300846e2c5c71
Tong Sun <suntong@cpan.org>
2018-12-31 21:48:59 -0500

    - [*] make it works

--- main.go
+++ main.go
@@ -1,109 +1,109 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/caarlos0/env"
-	"github.com/minio/minio-go"
-	"github.com/satori/go.uuid"
-	log "github.com/sirupsen/logrus"
-	tb "gopkg.in/tucnak/telebot.v2"
-	"net/http"
-	"net/url"
-	"os"
 	"strconv"
 	"time"
+
+	"github.com/caarlos0/env"
+	log "github.com/sirupsen/logrus"
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 type config struct {
-	TelegramToken  string `env:"TELEGRAM_TOKEN"`
-	TelegramUserID string `env:"TELEGRAM_USERID"`
-	MinioURL       string `env:"MINIO_URL"`
-	MinioAccessKey string `env:"MINIO_ACCESS_KEY"`
-	MinioSecretKey string `env:"MINIO_SECRET_KEY"`
+	TelegramToken  string `env:"SHUTTLEBOT_TOKEN,required"`
+	TelegramChatID string `env:"SHUTTLEBOT_CID,required"`
+	LogLevel       string `env:"SHUTTLEBOT_LOG"`
 }
 
 var (
-	versionflag bool
-	version     string
+	progname = "shuttlebot"
+	version  = "0.1.0"
+	date     = "2018-12-31"
 )
 
 func init() {
-	flag.BoolVar(&versionflag, "v", false, "Print build id")
-	flag.Parse()
+	log.SetFormatter(&log.TextFormatter{
+		TimestampFormat: "0102 15:04:05", // 2006-01-02
+		FullTimestamp:   true,
+	})
 }
 
 func main() {
-	if versionflag {
-		fmt.Printf("build : %s\n", version)
-		os.Exit(0)
-	}
+	app := Application{}
+	app.Run()
+}
+
+// Application holds things together
+type Application struct {
+	Chat []*tb.Chat
+	bot  *tb.Bot
+}
+
+// Run from start to end
+func (app *Application) Run() {
+	log.WithFields(log.Fields{
+		"version":  version,
+		"built-on": date,
+	}).Info("Telegram Forwarding Shuttle Bot")
 
 	c := config{}
 	err := env.Parse(&c)
 	if err != nil {
-		log.WithFields(log.Fields{"err": err}).Fatal("Can't parse config...")
+		log.WithFields(log.Fields{"err": err}).Fatal("Config parsing error")
 	}
-	b, err := tb.NewBot(tb.Settings{
+
+	bot, err := tb.NewBot(tb.Settings{
 		Token:  c.TelegramToken,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 	if err != nil {
-		log.WithFields(log.Fields{"err": err}).Fatal("Can't start bot. Check your config for telegram.")
+		log.WithFields(log.Fields{"err": err}).Fatal("Can't start bot")
 	}
-	b.Handle(tb.OnPhoto, func(m *tb.Message) {
-		userid, err := strconv.Atoi(c.TelegramUserID)
-		if err != nil {
-			log.WithFields(log.Fields{"err": err}).Fatal("TELEGRAM_USERID is not int")
-		}
-		if m.Sender.ID == userid {
-			if m.Photo.File.InCloud() {
-				fileurl, err := b.FileURLByID(m.Photo.File.FileID)
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID}).Fatal("Can't find file from telegram")
-					return
-				}
-				r, err := http.Get(fileurl)
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err}).Fatal("Can't download file from telegram")
-					b.Send(m.Sender, "Internal error :(")
-				}
-				s3Client, err := minio.New(c.MinioURL, c.MinioAccessKey, c.MinioSecretKey, true)
+	app.bot = bot
 
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err}).Fatal("Can't connect to S3")
-					b.Send(m.Sender, "Internal error :(")
-				}
-				// or error handling
-				uuid, err := uuid.NewV4()
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err}).Fatal("Something went wrong when you generate the uuid")
-					b.Send(m.Sender, "Internal error :(")
-				}
-				namefile := uuid.String() + ".jpg"
-				n, err := s3Client.PutObject("telegram", namefile, r.Body, r.ContentLength, minio.PutObjectOptions{ContentType: "application/octet-stream"})
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err}).Fatal("It's not possible to upload file to S3")
-					b.Send(m.Sender, "Internal error :(")
-				}
-				log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err, "s3": n}).Debug("Uploaded Successfully")
-				reqParams := make(url.Values)
+	log.Info("Copyright (C) 2018-2019, Tong Sun")
+	app.Chat = make([]*tb.Chat, 0)
+	gi, err := strconv.ParseInt("-"+c.TelegramChatID, 10, 64)
+	if err != nil {
+		log.WithFields(log.Fields{"err": err}).Fatal("Parse SHUTTLEBOT_CID error")
+	}
+	app.Chat = append(app.Chat, &tb.Chat{ID: gi})
 
-				presignedURL, err := s3Client.PresignedGetObject("telegram", namefile, time.Second*24*60*60, reqParams)
-				if err != nil {
-					log.WithFields(log.Fields{"iduser": m.Sender.ID, "err": err}).Fatal("Can't Presigned url")
-				}
-				log.WithFields(log.Fields{"iduser": m.Sender.ID, "url": presignedURL.String()}).Debug("Successfully generated presigned URL")
-				b.Send(m.Sender, presignedURL.String())
-			} else {
-				log.WithFields(log.Fields{"iduser": m.Sender.ID}).Warn("Error telegram upload")
-				b.Send(m.Sender, "Error telegram upload.")
-			}
+	bot.Handle(tb.OnText, app.ForwardHandler)
+	bot.Handle(tb.OnAudio, app.ForwardHandler)
+	bot.Handle(tb.OnContact, app.ForwardHandler)
+	bot.Handle(tb.OnDocument, app.ForwardHandler)
+	bot.Handle(tb.OnLocation, app.ForwardHandler)
+	bot.Handle(tb.OnPhoto, app.ForwardHandler)
+	bot.Handle(tb.OnVenue, app.ForwardHandler)
+	bot.Handle(tb.OnVideo, app.ForwardHandler)
+	bot.Handle(tb.OnVideoNote, app.ForwardHandler)
+	bot.Handle(tb.OnVoice, app.ForwardHandler)
+	// bot.Handle(tb.OnSticker, app.ForwardHandler)
 
-		} else {
-			log.WithFields(log.Fields{"iduser": m.Sender.ID}).Warn("Unauthorized user")
-			b.Send(m.Sender, "Unauthorized user")
-		}
-	})
-	b.Start()
+	// bot.Handle(tb.OnPinned, savePinnedMessage)
+	// bot.Handle(tb.OnAddedToGroup, showWelcomeMessage)
+
+	log.WithFields(log.Fields{"LogLevel": c.LogLevel}).Info("Running with")
+	if c.LogLevel == "Debug" {
+		log.SetLevel(log.DebugLevel)
+	} else if c.LogLevel == "Trace" {
+		log.SetLevel(log.TraceLevel)
+		log.SetReportCaller(true)
+	}
+
+	log.Info("Bot started")
+	bot.Start()
+}
+
+// ForwardHandler forwards received messages
+func (app *Application) ForwardHandler(message *tb.Message) {
+	log.WithFields(log.Fields{
+		"Sender": message.Sender,
+		"Title":  message.Chat.Title,
+		"Text":   message.Text,
+	}).Debug("Message received")
+	for _, chat := range app.Chat {
+		app.bot.Forward(chat, message)
+	}
 }
