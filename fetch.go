23f8a511c0de58305eaa1734e5fdd6c59385368d
Tong Sun <suntong@cpan.org>
2021-04-03 20:53:33 -0400

    - [*] convert non-concurrent code to concurrent code

--- fetch.go
+++ fetch.go
@@ -41,17 +41,20 @@ func (app *Application) FetchHandler(message *tb.Message) {
 	if debug >= 3 {
 		app.bot.Send(message.Chat, "Rogher @"+username)
 	}
-	r := app.Fetch(message.Payload, message.Chat)
+	r := <-app.Fetch(message.Payload, message.Chat)
 	if len(r) != 0 {
 		app.bot.Send(message.Chat, r+" @"+username)
 	}
 }
 
 // Fetch fetches from the given url and sent to TG group
-func (app *Application) Fetch(url string, Chat *tb.Chat) string {
+func (app *Application) Fetch(url string, Chat *tb.Chat) chan string {
+	r := make(chan string)
+	//defer close(r)
 	fileName := getFileName(url)
 	if fileName == "" {
-		return "Unknow url format"
+		r <- "Unknown url format"
+		return r
 	}
 
 	fileName += ".webm"
@@ -60,41 +63,47 @@ func (app *Application) Fetch(url string, Chat *tb.Chat) string {
 	args := []string{"-o", fileName, url}
 	args = append(args, cfg.Fetch.Vformat...)
 
-	cmd := exec.Command(cfg.Fetch.Downloader, args...)
-	err := cmd.Run()
-	if err != nil {
-		return err.Error()
-	}
+	go func() {
+		cmd := exec.Command(cfg.Fetch.Downloader, args...)
+		err := cmd.Run()
+		if err != nil {
+			r <- err.Error()
+			return
+		}
 
-	// get video duration
-	cmdStr := "ffprobe -i " + fileName +
-		" 2>&1 | sed -n '/^  *Duration: /{ s/^.*Duration: //; s/,.*$//; p; }'"
-	out, err := exec.Command("sh", "-c", cmdStr).Output()
-	if err != nil {
-		return err.Error()
-	}
-	d1, _ := time.Parse("15:04:05", string(out))
-	d0, _ := time.Parse("15:04:05", "00:00:00")
-	duration := int(d1.Sub(d0).Seconds())
+		// get video duration
+		cmdStr := "ffprobe -i " + fileName +
+			" 2>&1 | sed -n '/^  *Duration: /{ s/^.*Duration: //; s/,.*$//; p; }'"
+		out, err := exec.Command("sh", "-c", cmdStr).Output()
+		if err != nil {
+			r <- err.Error()
+			return
+		}
+		d1, _ := time.Parse("15:04:05", string(out))
+		d0, _ := time.Parse("15:04:05", "00:00:00")
+		duration := int(d1.Sub(d0).Seconds())
 
-	// Send out fileName
-	logIf(2, "Send-video",
-		"group", Chat.Title, "name", fileName)
+		// Send out fileName
+		logIf(2, "Send-video",
+			"group", Chat.Title, "name", fileName)
 
-	v := &tb.Video{
-		File:  tb.FromDisk(fileName),
-		Width: 640, Height: 360, Duration: duration,
-	}
-	if debug >= 3 {
-		fmt.Printf("] %#v\n", v)
-	}
-	m, err := app.bot.Send(Chat, v)
-	if err != nil {
-		return err.Error()
-	}
-	logIf(2, "Sent-video", "id", m.ID)
+		v := &tb.Video{
+			File:  tb.FromDisk(fileName),
+			Width: 640, Height: 360, Duration: duration,
+		}
+		if debug >= 3 {
+			fmt.Printf("] %#v\n", v)
+		}
+		m, err := app.bot.Send(Chat, v)
+		if err != nil {
+			r <- err.Error()
+			return
+		}
+		logIf(2, "Sent-video", "id", m.ID)
 
-	return ""
+		r <- ""
+	}()
+	return r
 }
 
 func getFileName(url string) string {
